6cd5b6ac00f766d6d7698f7b4872ef09947d2c9c
diwe innocent <diweesomchi@gmail.com>
2023-08-08 12:17:49 +0100

    new changes on design

--- excelsior/settings.py
+++ excelsior/settings.py
@@ -94,22 +94,22 @@ WSGI_APPLICATION = 'excelsior.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'esomchi$excelroyal',
-        'USER': 'esomchi',
-        'PASSWORD': 'excelsior',
-        'HOST': 'esomchi.mysql.pythonanywhere-services.com',
-        'PORT': '3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'esomchi$excelroyal',
+#         'USER': 'esomchi',
+#         'PASSWORD': 'excelsior',
+#         'HOST': 'esomchi.mysql.pythonanywhere-services.com',
+#         'PORT': '3306'
+#     }
+# }
 
 
 # Password validation
