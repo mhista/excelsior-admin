8e09332018827cfb6b1780d31ebea5d8ea54dc9b
diwe innocent <diweesomchi@gmail.com>
2023-08-08 10:00:10 +0100

    added a requirement.txt file

--- excelsior/settings.py
+++ excelsior/settings.py
@@ -94,22 +94,22 @@ WSGI_APPLICATION = 'excelsior.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'parizian',
-#         'USER': 'root',
-#         'PASSWORD': '@Diwe07068884102',
-#         'HOST': 'localhost',
-#         'PORT': '3306'
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'excelroyal',
+        'USER': 'root',
+        'PASSWORD': 'excelsior',
+        'HOST': 'localhost',
+        'PORT': '3306'
+    }
+}
 
 
 # Password validation
